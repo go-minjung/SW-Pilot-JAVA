874a93e43ba2e3af2316f93b178a88e98c7af832
go-minjung <a01086712346@gmail.com>
2025-05-15 00:06:23 +0900

    solve/course2-problem2

--- Course2/src/RoadToBiodome02.java
+++ Course2/src/RoadToBiodome02.java
@@ -1,5 +1,6 @@
-// 입력된 문장의 각 문자를 역순으로 출력 (배열)
+// 입력된 문장의 각 문자를 역순으로 출력
 public class RoadToBiodome02 {
+    // stack 클래스 (배열)
     static class CharStack {
         private char[] stack;
         private int top;
@@ -12,7 +13,7 @@ public class RoadToBiodome02 {
 
         // push
         public void push(char c) {
-            if (top <= stack.length - 1) {
+            if (top >= stack.length - 1) {
                 System.out.println("스택이 가득 찼습니다.");
                 return;
             }
@@ -44,6 +45,26 @@ public class RoadToBiodome02 {
         for (String arg : args) {
             inputBuilder.append(arg).append(" ");
         }
+        String input = inputBuilder.toString().trim();
 
+        // 입력 검사
+        if (input.trim().isEmpty() || input.length() < 2 || input.length() > 1000000) {
+            System.out.println("입력된 메시지가 올바르지 않습니다. 다시 한번 확인해주세요.");
+            return;
+        }
+
+        // stack 생성
+        CharStack stack = new CharStack(input.length());
+
+        // 문자 push
+        for (int i=0; i<input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        // pop하여 역순 출력
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+        System.out.println();
     }
 }
